08a33b385bbede9c3b97e68efc0ab6bf3cf65587
benjamin <benjamin.cho@gurunfin.com>
2024-08-13 13:57:06 +0900

    fix test error

--- src/board/board.controller.spec.ts
+++ src/board/board.controller.spec.ts
@@ -1,15 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BoardController } from './board.controller';
+import { BoardService } from './board.service';
 
 describe('BoardController', () => {
   let controller: BoardController;
+  let service: BoardService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BoardController],
+      providers: [
+        {
+          provide: BoardService,
+          useValue: {
+            AllBoards: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),            
+          }
+        }
+      ]
     }).compile();
 
     controller = module.get<BoardController>(BoardController);
+    service = module.get<BoardService>(BoardService);
   });
 
   it('should be defined', () => {
